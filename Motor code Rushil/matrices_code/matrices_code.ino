17ac0c23414377412612dec66150cea46c43f9ea
varshneyrushil <45682526+varshneyrushil@users.noreply.github.com>
2018-12-10 17:57:25 +0530

    Add files via upload

---
+++ Motor code Rushil/matrices_code/matrices_code.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*Algorithm: 
+   * make 4 matrices corresponding to the four quadrants
+   * the matrices should be of the form A[y][x] where x and y are the coordinates
+   * divide each matrix into three cases: where one of the motors has a constant direction, the other motor has a varying
+   * direction depending on the segment the cell corresponding to the joystick value is in
+   * map values for direction and pwm signal.
+   * depending on input value, call out corresponding vals and do analog and digital writes to the motors for various 
+   * values of the joystick.
+   */
+}
---
+++ Motor code Rushil/matrix_code(rushil)/matrix_code.ino
@@ -0,0 +1,44 @@
+/*code for matrix
+ * matrix : A[y][x]
+ */
+ struct node
+ {
+  bool dirr;bool dirl;
+  int r;
+  int l;
+ }
+node a[512][512];
+void setup() {
+  // put your setup code here, to run once:
+  for(int i=0;i<512;i++)
+  {
+    for(int j=0;j<512;j++)
+    {
+      if(i==j)
+      {
+        dirr = 1; dirl = 0;
+        r = map(i,0,512,255,0);
+        l = 0;
+      }
+      if(i<j)
+      {
+        dirr = 1; dirl = 1;
+       r = map(i,0,512,255,0);
+       l = map(j,0,512,0,255);
+      }
+      if(i>j)
+      {
+        dirr = 1; dirl = 0;
+        r = map(j,0,512,255,0);
+        l = map(i,0,512,255,0);
+      }
+    }
+  }
+
+}
+//make a function to retrieve values and for analogWrite
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
